5a47684a050a3b570340360926c0b6414a82d301
jorgeshadow <42231910+jorgeshadow@users.noreply.github.com>
2018-12-04 20:41:32 -0800

    funciones agregar borrar modificar y cancelar

--- AlmacenTecSP/MainWindow.xaml.cs
+++ AlmacenTecSP/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace AlmacenTecSP
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        /// <summary>
+        /// guardar campos llenos con el botn guardar
+        /// <summary
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            this.dc.SubmitChanges();
+            isAdd = false;
+        }
+        /// <summary>
+        /// agregar nuevo producto
+        /// </summary>
+         private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            isAdd = true;
+            Customer customer = (Customer)(this.CustomerView.AddNew());
+            customer.CustomerID = "New";
+            customer.ContactName = "";
+            customer.City = "";
+            customer.CompanyName = "";
+            customer.Address = "";
+ 
+            this.CustomerView.CommitNew();
+            dgCustomers.ScrollIntoView(customer);
+ 
+        }
+        /// <summary>
+        /// borrar un registro de la tabla
+        /// </summary>
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("?", "Delete", MessageBoxButton.YesNoCancel) == MessageBoxResult.Yes)
+            {
+                this.CustomerView.RemoveAt(this.CustomerView.CurrentPosition);
+                this.dc.SubmitChanges();
+            }
+ 
+            isAdd = false;
+        }
+        //cancelar
+         private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            if(isAdd)
+            {
+                CustomerView.CancelNew();
+                CustomerView.Remove(this.CustomerView.CurrentItem);
+            }
+            else
+            {
+                CustomerView.CancelEdit();
+                dc.Refresh(System.Data.Linq.RefreshMode.OverwriteCurrentValues);
+                CustomerView.Refresh();
+            }
         }
     }
 }
