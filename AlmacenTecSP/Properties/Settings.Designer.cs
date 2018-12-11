d56fb7a84253ac065859f4f23ee4ad045bb321c2
jorgeshadow <42231910+jorgeshadow@users.noreply.github.com>
2018-12-11 11:04:49 -0800

    models

--- AlmacenTecSP/Properties/Settings.Designer.cs
+++ AlmacenTecSP/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace AlmacenTecSP.Properties {
                 return ((string)(this["AlmacenTecSPConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\BD\\AlmacenTec" +
+            "SP.mdf;Integrated Security=True")]
+        public string AlmacenTecSPConnectionString1 {
+            get {
+                return ((string)(this["AlmacenTecSPConnectionString1"]));
+            }
+        }
     }
 }
